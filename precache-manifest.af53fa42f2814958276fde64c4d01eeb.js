2a881d95266cf638c965dd732f784d04d9051965
mattanglin <mattanglin@gmail.com>
2021-05-26 00:01:40 +0000

    Deploying to gh-pages from @ mattanglin/daily-todos@a903c0a946704c91e6ef96586416862ee488ef20 🚀

---
+++ precache-manifest.af53fa42f2814958276fde64c4d01eeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcb1b7f198cdbf9d20cb591b0971d59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad47d3eee371934e25fe",
+    "url": "./static/js/2.5b0bb4b8.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "./static/js/2.5b0bb4b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce931143c25f5bd06bc4",
+    "url": "./static/js/main.4c25d56e.chunk.js"
+  },
+  {
+    "revision": "0aa32fd1a799b37b43ba",
+    "url": "./static/js/runtime-main.c24db51d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1b0b5c65639f901ba84f73a83f058da.js"
+  "./precache-manifest.af53fa42f2814958276fde64c4d01eeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
